bacb6344731cb24907101a294f35ee7c79b02050
Bogdan B-R <brir0567@scs.ubbcluj.ro>
2012-04-22 04:03:38 +0300

    Thumbnailer works.

--- Thumbnailer.d
+++ Thumbnailer.d
@@ -1,4 +1,5 @@
 import std.file;
+import Log;
 
 class Thumbnailer
 {
@@ -6,7 +7,14 @@ class Thumbnailer
   {
     if (std.file.exists(imageFilename))
     {
-      
+      string commandLines = std.string.format(`convert -define jpeg:size=800x800  %s  -auto-orient \
+          -thumbnail 400x400   -unsharp 0x.5  %s.gif && \
+convert -page +4+4 %s.gif -matte \
+          \( +clone -background black -shadow 60x4+4+4 \) +swap \
+          -background none -mosaic     %s`, 
+					     imageFilename, imageFilename, imageFilename, thumbnailFilename);
+      std.process.system(commandLines);
+      info(commandLines);
     }
   }
 }
