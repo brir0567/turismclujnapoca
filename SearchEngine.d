b6a5f3d22044abff67531ad41223eacf611dccf4
Bogdan B-R <brir0567@scs.ubbcluj.ro>
2012-05-05 19:23:47 +0300

    Generated robots.txt.

--- SearchEngine.d
+++ SearchEngine.d
@@ -1,9 +1,11 @@
+import FileUtils;
+
 class SearchEngine
 {  
   public void generateFilesForCrawler(string[] directoriesList)
   {
     generateSiteMapXml(directoriesList);
-    generateRobotsTxt(directoriesList);
+    generateRobotsTxt();
   }
 
   public void generateSiteMapXml(string[] directoriesList)
@@ -11,9 +13,11 @@ class SearchEngine
 
   }
 
-  public void generateRobotsTxt(string[] directoriesList)
+  public void generateRobotsTxt()
   {
-
+    string content = "Sitemap: http://www.scs.ubbcluj.ro/~brir0567/sitemap.xml";
+    FileUtils fileUtils = new FileUtils();
+    fileUtils.WriteContentToFile("robots.txt", content);
   }
 
 }
